92f203e7ce346964481ea018dffb14b714df6978
Júnior Nascimento <junior.nascm@gmail.com>
2023-03-30 10:26:39 -0300

    push

---
+++ main.py
@@ -0,0 +1,35 @@
+from PIL import Image
+from sys import argv
+
+fileName = "input.txt" # path of your text file
+
+try:
+    txt=open(argv[1], "r")
+except IndexError:
+    print("No file entered. Using default file...")
+    txt=open(fileName, "r")
+except FileNotFoundError:
+    print("Could not find file. Using default file...")
+    txt=open(fileName, "r")   
+
+BG=Image.open("images/bg.png") 
+sheet_width=BG.width
+gap, ht = 80, 80
+for i in txt.read():
+    if i == '\n':
+        gap = 0;
+        ht = ht + 200
+        continue
+
+    cases = Image.open("images/{}.png".format(str(ord(i))))
+    BG.paste(cases, (gap, ht), cases)
+    size = cases.width
+    height=cases.height
+    #print(size)
+    gap+=size
+    if sheet_width < gap or len(i)*115 >(sheet_width-gap):
+        gap=0
+        ht= ht + 200
+print(gap)
+print(sheet_width)
+BG.show()
