f3533202d590fe3902462ac16340604b5befb4aa
Júnior Nascimento <junior.nascm@gmail.com>
2023-04-12 21:46:36 -0300

    separating syllabes

--- main.py
+++ main.py
@@ -2,31 +2,32 @@ from PIL import Image
 from sys import argv
 import random
 import hyphen
-
-h = hyphen.Hyphenator('pt_BR')
-
-fileName = "input.txt" # path of your text file
+import string
+import re
 
 DEFAULT_GAP = 300
 DEFAULT_HT = 100
-
-gap, ht = DEFAULT_GAP, DEFAULT_HT
-
 LETTER_WIDTH = 50
 LETTER_HEIGHT = 75
 
+h = hyphen.Hyphenator('pt_BR')
+fileName = "input.txt" # path of your text file
+gap, ht = DEFAULT_GAP, DEFAULT_HT
+pages = []
+current_page = 0
+
 def get_leter(letter):
-    cases = Image.open("images/{}.png".format(str(ord(i))))
+    cases = Image.open("images/{}.png".format(str(ord(letter))))
     cases.thumbnail((50,75), Image.Resampling.LANCZOS)
     angle = random.randint(-15, 15)
     rt = cases.rotate(angle)
     return rt
 
 def print_letter(image, letter):
-    rt = get_leter(i)
+    rt = get_leter(letter)
     x = random.randint(-20, -20)
     y = random.randint(-20, -20)
-    BG.paste(rt, (gap + x, ht + y), rt)
+    image.paste(rt, (gap + x, ht + y), rt)
     size = rt.width
     height = rt.height
     return (size, height)
@@ -36,32 +37,82 @@ def breakline(gap, ht):
     ht = ht + DEFAULT_HT
     return gap, ht
 
-try:
-    txt=open(argv[1], "r")
-except IndexError:
-    print("No file entered. Using default file...")
-    txt=open(fileName, "r")
-except FileNotFoundError:
-    print("Could not find file. Using default file...")
-    txt=open(fileName, "r")   
+def open_file():
+    try:
+        return open(argv[1], "r")
+    except IndexError:
+        print("No file entered. Using default file...")
+        return open(fileName, "r")
+    except FileNotFoundError:
+        print("Could not find file. Using default file...")
+        return open(fileName, "r")   
 
-BG=Image.open("images/bg2.png") 
-sheet_width=BG.width
-brkline = False
+def get_background():
+    return Image.open("images/bg2.png").convert('RGB')
 
-for w in txt.read().split():
-    for idx, s in enumerate(h.syllables(w)): # syllabes of current word
-        if len(s) * LETTER_WIDTH > (sheet_width - gap):
-                if idx == 0:
-                    continue
-                s = '-' + s # hyphen
-                brkline = True
-        for i in s:
-            size, height = print_letter(BG, i)
-            gap += size
-            if LETTER_WIDTH > (sheet_width - gap) or brkline:
+def syllable_fit_in_line(syllable, gap, sheet_width):
+    syllable_length = (len(syllable) * LETTER_WIDTH) + 200
+    current_line_width = gap
+    return syllable_length < (sheet_width - current_line_width)
+
+def fit_in_page(ht, sheet_height):
+    return ht < (sheet_height - DEFAULT_HT * 2)
+
+def new_page(current_page):
+    pages.append(background.copy())
+    current_page = current_page + 1
+    return current_page, DEFAULT_HT, DEFAULT_GAP
+
+def print_pdf():
+    pages[0].save("output.pdf", save_all=True, append_images=pages[1:])
+
+def print_space(gap):
+    gap = gap + LETTER_WIDTH + random.randint(-5,5)
+    return gap
+txt = open_file()
+background = get_background()
+pages.append(background.copy())
+
+lines = txt.readlines()
+
+for line in lines:
+    for word in line.split():
+        punctuation = None
+        if word[-1] in string.punctuation:
+            punctuation = word[-1]
+            word = word[:-1]
+
+        syllables = None
+        if len(h.syllables(word)) < 1:
+            syllables = [word]
+        else:
+            syllables = h.syllables(word)
+
+        for index, syllable in enumerate(syllables):
+            if syllable_fit_in_line(syllable, gap, background.width):
+                for letter in syllable:
+                    print_letter(pages[current_page], letter)
+                    gap = print_space(gap)
+            else:
+                if not fit_in_page(ht, background.height):
+                    current_page, ht, gap = new_page(current_page)
+                if index == 0 and (len(syllables) > 1):
+                    syllabe = syllable + '-'
+                else:
+                    syllabe = '-' + syllable
+                
+                for letter in syllabe:
+                    print_letter(pages[current_page], letter)
+                    gap = print_space(gap)
+                
                 gap, ht = breakline(gap, ht)
-                brkline = False
-    gap += LETTER_WIDTH # space after word
-BG.show()
 
+        if punctuation is not None:
+            print_letter(pages[current_page], punctuation)
+            gap = print_space(gap)
+ 
+        gap = print_space(gap)
+
+    gap, ht = breakline(gap, ht)
+
+print_pdf()
